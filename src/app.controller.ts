eb26e5388c26eaf21399488f2906762ba40c37c0
Sandun Tharuka <sandun.suntws@gmail.com>
2025-08-28 23:07:14 +0530

    setup nodemailer

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { SendMailDto } from './mailer/send-mail.dto';
 import { ApiKeyGuard } from './common/guards/api-key-guard';
@@ -6,6 +6,11 @@ import { ApiKeyGuard } from './common/guards/api-key-guard';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return 'hELLOW WORLD!';
+  }
 
   @Post('send-mail')
   @UseGuards(ApiKeyGuard)
